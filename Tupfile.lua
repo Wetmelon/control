16f3ba6749e9300028b9c0f898caedfbd7c90795
Paul Guenette <pfguenette@gmail.com>
2022-01-23 21:52:03 -0500

    Build tests

--- Tupfile.lua
+++ Tupfile.lua
@@ -1,2 +1,2 @@
 
-tup.foreach_rule('test/*.cpp', 'g++ -O3 -std=c++17 -c %f -o %o', 'test/build/obj/%B.o')
+tup.foreach_rule('test/*.cpp', 'g++ -I. -O3 -std=c++17 -c %f -o %o', 'test/build/obj/%B.o')
