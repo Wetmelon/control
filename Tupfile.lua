5096804db60f3b77f386844f164e39c8ec8b1345
Paul Guenette <pfguenette@gmail.com>
2025-05-22 00:03:05 -0700

    Update tupfile

--- Tupfile.lua
+++ Tupfile.lua
@@ -1,11 +1,28 @@
 INCLUDES = "-I. -Isource -Ieigen"
 WARNINGS = '-Wall -Wextra'
 
--- Compile all objects
-test_objs = tup.foreach_rule('source/*.cpp', '^j^g++ '..INCLUDES..' -O3 '..WARNINGS..' -std=c++23 -c %f -o %o', 'build/obj/%B.o')
+COMMON_FLAGS = '-O2'
+CCFLAGS = '-std=c17'
+CXXFLAGS = '-std=c++23'
+LDFLAGS = '-Wl,--gc-sections -static'
 
--- Generate test executable
-test_runner = tup.rule(test_objs, 'g++ %f -lstdc++exp -o %o', 'build/test_runner.exe')
+CC_PATH = tup.getconfig("COMPILER_PATH")
+if CC_PATH ~= "" then
+    print("Compiler Path: "..CC_PATH)
+end
+
+if tup.getconfig('LTO') == 'true' then
+    COMMON_FLAGS += ' -flto'
+    LDFLAGS += ' -flto'
+end
+
+-- Compile all objects
+objs = tup.foreach_rule('source/*.c', '^j^'..CC_PATH..'gcc '..INCLUDES..' '..COMMON_FLAGS..' '..CCFLAGS..' '..WARNINGS..' -c %f -o %o', 'build/obj/%B.o')
+objs = tup.foreach_rule('source/*.cpp', '^j^'..CC_PATH..'g++ '..INCLUDES..' '..COMMON_FLAGS..' '..CXXFLAGS..' '..WARNINGS..' -c %f -o %o', 'build/obj/%B.o')
 
 -- Generate assembly for each object
-tup.foreach_rule(test_objs, 'objdump -dSC %f > %o', 'build/asm/%B.asm')
+tup.foreach_rule(objs, CC_PATH..'objdump -dC %f > %o', 'build/asm/%B.asm')
+
+-- Generate test executable
+test_runner = tup.rule(objs, 'g++ '..LDFLAGS..' %f -lstdc++exp -o %o', 'build/test_runner.exe')
+
